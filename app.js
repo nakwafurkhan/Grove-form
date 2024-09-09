ff278b1c52b8a2b2676d0981dfcf295fd6d2eee9
nakwafurkhan <79127004+nakwafurkhan@users.noreply.github.com>
2024-09-09 15:55:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var bodyvar = document.body
+var button = document.getElementById("butt")
+console.log(button)
+var thanks = document.getElementById("thanks")
+button.onclick=()=>{
+button.style.display='none'
+thanks.style.display='block'
+}
